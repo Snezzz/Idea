f035c73c23a39f6c4b95c7a15a31baa32a27e8fe
RinaKend <49126781+RinaKend@users.noreply.github.com>
2019-04-03 21:03:06 +0300

    Update Application.java
    Вызов теста из main

--- src/main/java/com/sweater/Application.java
+++ src/main/java/com/sweater/Application.java
@@ -7,6 +7,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args); //запуск
+         TestLombok.Testing();//тест
     }
 
 }
