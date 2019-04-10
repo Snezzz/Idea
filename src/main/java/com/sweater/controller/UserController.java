193f9eaca240c586e8c49c9d1f33089fb7af4e84
Снежа <snezha_00_97@mail.ru>
2019-04-10 22:41:25 +0300

    Посмотрела видео и переделала в нужный формат

---
+++ src/main/java/com/sweater/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.sweater.controller;
+
+import com.sweater.entities.User;
+import com.sweater.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+    @PostMapping("/user")
+    public void addUser(@RequestBody User user){
+        userService.addUser(user);
+    }
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestBody User user){
+        userService.deleteUser(user);
+    }
+    @DeleteMapping("/userById")
+    public void deleteUserById(@RequestBody int id){
+        userService.deleteUserById(id);
+    }
+
+}
--- src/main/java/com/sweater/repositories/NotesRepository.java
+++ src/main/java/com/sweater/repositories/NotesRepository.java
@@ -1,11 +1,11 @@
 package com.sweater.repositories;
 
 import com.sweater.entities.Notes;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface NotesRepository extends CrudRepository<Notes, Integer> {
+public interface NotesRepository extends JpaRepository<Notes, Integer> {
 
 }
