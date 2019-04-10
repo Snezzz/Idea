dcd5ac0dce38a232bb3bff79e72c876f92204bfb
RinaKend <irun2704@yandex.ru>
2019-04-11 01:34:18 +0300

    Сделана аутентификация spring security basic, добавлен тестовый юзер имя tom, пароль 123

---
+++ src/main/java/com/sweater/WebSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.sweater;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        String password = "123";
+
+        String encrytedPassword = this.passwordEncoder().encode(password);
+        System.out.println("Encoded password of 123=" + encrytedPassword);
+
+
+        InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> //
+                mngConfig = auth.inMemoryAuthentication();
+
+        // Defined  user in memory
+
+        // Spring auto add ROLE_
+        UserDetails u1 = User.withUsername("tom").password(encrytedPassword).roles("USER").build();
+        mngConfig.withUser(u1);
+    }
+}
