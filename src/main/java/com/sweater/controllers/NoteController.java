905125637d8e57e18b4c8a0b971eef161475f7e1
RinaKend <irun2704@yandex.ru>
2019-04-11 02:40:17 +0300

    NoteService update controller

--- src/main/java/com/sweater/controllers/NoteController.java
+++ src/main/java/com/sweater/controllers/NoteController.java
@@ -4,6 +4,8 @@ import com.sweater.entities.Notes;
 import com.sweater.repositories.NotesRepository;
 import com.sweater.services.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -17,31 +19,36 @@ public class NoteController {
         this.noteService = noteService;
     }
 
-    @GetMapping("/notes")
-    public List<Notes> getAllNotes() {
-        return noteService.getAllNotes();
+    @GetMapping("notes")
+    public ResponseEntity<List<Notes>> getAllNotes() {
+        List<Notes> list = noteService.getAllNotes();
+        return new ResponseEntity<List<Notes>>(list, HttpStatus.OK);
     }
 
 
-    @GetMapping("/noteById")
-    public Notes getNoteById(@RequestBody int id) {
-        return noteService.getNoteById(id);
-    }
-
-    @PostMapping("/note")
-    public void addNote(@RequestBody Notes note) {
-        noteService.addNote(note);
+    @GetMapping("note/{id}")
+    public ResponseEntity<Notes> getNoteById(@PathVariable("id") Integer id) {
+        Notes article = noteService.getNoteById(id);
+        return new ResponseEntity<Notes>(article, HttpStatus.OK);
     }
 
     @PutMapping("note")
-    public void updateNote(@RequestBody Notes note) {
-        noteService.updateNote(note);
+    public ResponseEntity<Notes> updateNote(@RequestBody Notes article) {
+        noteService.updateNote(article);
+        return new ResponseEntity<Notes>(article, HttpStatus.OK);
     }
 
-
-    @DeleteMapping("/noteById")
-    public void deleteNote(@RequestBody int id) {
+    @DeleteMapping("note/{id}")
+    public ResponseEntity<Void> deleteNote(@PathVariable("id") Integer id) {
         noteService.deleteNote(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
+    @PostMapping("note")
+    public ResponseEntity<Notes> addNote(@RequestBody Notes note) {
+        noteService.addNote(note);
+        return new ResponseEntity<Notes>(note, HttpStatus.OK);
 
+
+
+    }
 }
