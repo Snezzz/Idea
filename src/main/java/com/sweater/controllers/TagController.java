9dc9a6319381952eba65c5d88168c767e27e0351
Снежа <snezha_00_97@mail.ru>
2019-04-16 13:08:43 +0300

    Corrected the structure of the project (there were folders 'controllers' VS 'controller', 'services' VS 'service')

--- src/main/java/com/sweater/controllers/TagController.java
+++ src/main/java/com/sweater/controllers/TagController.java
@@ -17,30 +17,35 @@ public class TagController {
         this.tagService = tagService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     //выводит все тэги
     @GetMapping("/tags")
     public List<Tag> getTags() {
         return tagService.getTags();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     //выводит тэг по идентификатору
     @GetMapping("/tag/{id}")
     public Tag getTagById(@PathVariable int id) {
         return tagService.getTagById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     //добавляет новый тэг
     @PostMapping("/tag")
     public void addTag(@RequestBody Tag tag) {
         tagService.addTag(tag);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     //удаляет тэг
     @DeleteMapping("/tag")
     public void deleteTag(@RequestBody Tag tag) {
         tagService.deleteTag(tag);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     //удаляет тэг по идентификатору
     @DeleteMapping("/tag/{id}")
     public void deleteTagById(@PathVariable int id) {
--- src/main/java/com/sweater/controllers/UserController.java
+++ src/main/java/com/sweater/controllers/UserController.java
@@ -18,22 +18,31 @@ public class UserController {
         this.userService = userService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/users")
     public List<User> getUsers(){
         return userService.getUsers();
     }
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable int id){
         return userService.getUserById(id);
     }
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/user")
     public void addUser(@RequestBody User user){
         userService.addUser(user);
     }
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/user")
     public void deleteUser(@RequestBody User user){
         userService.deleteUser(user);
     }
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/user/{id}")
     public void deleteUserById(@PathVariable int id){
         userService.deleteUserById(id);
