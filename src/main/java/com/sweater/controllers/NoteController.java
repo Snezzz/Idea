3997fc670b1792219629306d7f8ed078e0621b03
Снежа <snezha_00_97@mail.ru>
2019-04-18 23:33:12 +0300

    Added @CrossOrigin to NoteController haven't conflicts

--- src/main/java/com/sweater/controllers/NoteController.java
+++ src/main/java/com/sweater/controllers/NoteController.java
@@ -1,13 +1,10 @@
 package com.sweater.controllers;
 
 import com.sweater.entities.Notes;
-import com.sweater.repositories.NotesRepository;
 import com.sweater.services.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -19,33 +16,32 @@ public class NoteController {
         this.noteService = noteService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("notes")
-    
     public List<Notes> getAllNotes() {
         return noteService.getAllNotes();
     }
 
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("note/{id}")
-    
     public Notes getNoteById(@PathVariable("id") Integer id) {
         return noteService.getNoteById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PutMapping("note")
- 
     public void updateNote(@RequestBody Notes note) {
         noteService.updateNote(note);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("note/{id}")
-   
     public void  deleteNote(@PathVariable("id") Integer id) {
         noteService.deleteNote(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("note")
-    
     public void addNote(@RequestBody Notes note) {
         noteService.addNote(note);
     }
